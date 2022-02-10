c1f18a2d3ff935fc235b55250ba734bc395a3f6e
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2022-02-10 15:39:41 -0300

    Implementation of two methods

--- play-store-scraper-api/app.py
+++ play-store-scraper-api/app.py
@@ -1,5 +1,10 @@
-from google_play_scraper import app as play_scraper
+from google_play_scraper import app as play_scraper, Sort, reviews
 from flask import Flask
+
+import storeconfig as cfg
+import json
+
+APPLICATION_ID = cfg.stores["google"]
 
 app = Flask(__name__)
 
@@ -9,4 +14,28 @@ def status_api():
 
 @app.route("/app/details")
 def app_details():
-    return play_scraper("applicationId") 
+    scrap_result = play_scraper(APPLICATION_ID)
+
+    data = {}
+    data['installs'] = scrap_result['installs']
+    data['minVersion'] = scrap_result['androidVersion']
+    data['size'] = scrap_result['size']
+
+    return json.dumps(data)
+
+
+@app.route("/app/reviews")
+def app_reviews():
+    result, continuation_token = reviews(
+        APPLICATION_ID,
+        sort=Sort.MOST_RELEVANT, # defaults to Sort.MOST_RELEVANT
+        count=3, # defaults to 100
+        filter_score_with=5 # defaults to None(means all score)
+    )
+
+    data_length = len(result)
+
+    if data_length > 0:
+        return result
+    else:
+        return json.dumps([])
